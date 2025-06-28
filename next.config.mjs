d9553d39266625cfe4721f44d81936730f4bd1bb
Macbook Air <macbookair@Obinnas-MacBook-Air.local>
2025-06-28 22:07:16 +0100

    redirect fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          has: [
+            {
+              type: 'host',
+              value: 'fmccng.com',
+            },
+          ],
+          destination: 'https://www.fmccng.com',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
